52cc4ff1154a94ed6c6d4b4e15ae10cc35406c71
Susanna <syuzi22@gmail.com>
2020-11-03 18:15:05 +0300

    Added typings

---
+++ types/jqueryslider/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for jQuery Slider plugin
+// Project: https://github.com/syuzi22/slider
+// TypeScript Version: 2.3
+
+/// <reference types="jquery" />
+import 'jquery'
+
+interface Jquery {
+    slider(options: object): JQuery;
+}
