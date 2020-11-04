ae253e70c5910f2a30c2c2b048da96e6313794df
Susanna <syuzi22@gmail.com>
2020-11-04 16:11:22 +0300

    Split demo and the lib into separate entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require("webpack");
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: ['@babel/polyfill', './src/index.ts'],
+  entry: {
+      slider: ['@babel/polyfill', './src/index.ts'],
+      demo: ['@babel/polyfill', './src/index.ts', './src/demo/demo.ts'],
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name]/[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
@@ -15,27 +19,35 @@ module.exports = {
   },
   devServer: {
     port: 4200,
-    index: "./index.html",
+    index: './demo/index.html',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html'
+      filename: 'demo/index.html',
+      template: 'src/demo/index.html',
+      chunks: ['demo']
     }),
     new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
+        $: 'jquery',
+        jQuery: 'jquery',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: '[name]/[name].css',
     }),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {},
+            },
+            'css-loader'
+        ],
       },
       {
         test: /\.ts$/,
